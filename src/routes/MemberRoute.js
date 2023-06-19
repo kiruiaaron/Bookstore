3c5e778eaae1c2f0049e988b07b20cd6e987ef9c
Mwangimark <markcosmars2000@gmail.com>
2023-06-19 12:21:50 +0300

    Member functions

--- src/routes/MemberRoute.js
+++ src/routes/MemberRoute.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const {
+    connectToDatabase,
+    getMember, getMemberId,
+    createNewMember,
+    getMembersWithBorrowedBook,
+    loginMember
+} = require('../controllers/MemberController')
 
 const MemberRouter = express.Router();
-const {} =require('../controllers/MemberController')
 
+MemberRouter.get('/', connectToDatabase)
+MemberRouter.get('/members', getMember)
+MemberRouter.get('/members/:id', getMemberId)
+MemberRouter.get('/loan', getMembersWithBorrowedBook)
+MemberRouter.post('/newmember', createNewMember)
 
-module.exports = MemberRouter;
+MemberRouter.get('/login',loginMember)
+
+module.exports = { MemberRouter };
